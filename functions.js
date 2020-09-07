9b98031e9917cfd7a428e91c65019c07a544f7b8
miladezzat <miladezzat.f@gmail.com>
2020-09-07 21:25:30 +0200

    functions part 2

--- functions.js
+++ functions.js
@@ -11,3 +11,12 @@ print(factorial(4));
 print(factorial(5));
 print(factorial(10));
 print(factorial(0));
+
+function curve(arr, amount) {
+    for (var i = 0; i < arr.length; ++i) {
+    arr[i] += amount;
+    }
+    }
+    var grades = [77, 73, 74, 81, 90];
+    curve(grades, 5);
+    print(grades); // displays 82,78,79,86,95
