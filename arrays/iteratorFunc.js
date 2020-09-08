f70be2872e23aa11353f8c04f05c68814432f0a0
miladezzat <miladezzat.f@gmail.com>
2020-09-08 20:49:32 +0200

    every function

--- arrays/iteratorFunc.js
+++ arrays/iteratorFunc.js
@@ -6,3 +6,17 @@ function square(num) {
 
 var nums = [1,2,3,4,5,6,7,8,9,10];
 nums.forEach(square);
+
+//every
+
+function isEven(num) {
+    return num % 2 === 0;
+}
+
+var nums = [2,4,6,8,10];
+var even = nums.every(isEven);
+if(even){
+    print('all numbers are even');
+} else{
+    print('not all numbers are even');
+}
