d608cbda2c22f7d678333db32d7d57066dd931ba
miladezzat <miladezzat.f@gmail.com>
2020-09-08 09:28:15 +0200

    mutator functions in arrays

---
+++ arrays/mutatorFunctions.js
@@ -0,0 +1,11 @@
+var arr = [2,3,4,5];
+print(arr); // print 2,3,4,5
+arr.shift(); // pop first number 2 
+print(arr); //print 3,5,5
+arr.unshift(1,2);
+print(arr); // 1,2,3,4,5
+arr.pop(); // pop last number5
+print(arr);//1,2,3,4
+
+arr.push(5) // add 5 in last element
+print(arr) //1,2,3,4,5
