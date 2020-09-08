d8a1366eb5e0e1b1bce91a17dd3895b2912e5fa2
miladezzat <miladezzat.f@gmail.com>
2020-09-08 09:10:05 +0200

    indexOf

---
+++ arrays/indexOf.js
@@ -0,0 +1,9 @@
+var names = ["David", "Cynthia", "Raymond", "Clayton", "Jennifer"];
+
+var position = names.indexOf('Raymond');
+if (position >= 0) {
+    print("Found " + 'Raymond' + " at position " + position);
+}
+else {
+    print('Raymond ' + " not found in array.");
+}
