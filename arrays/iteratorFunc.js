33d0a941e514dc4ae41703b9b362c0460e9d00ad
miladezzat <miladezzat.f@gmail.com>
2020-09-08 20:52:34 +0200

    reduce function

--- arrays/iteratorFunc.js
+++ arrays/iteratorFunc.js
@@ -20,3 +20,13 @@ if(even){
 } else{
     print('not all numbers are even');
 }
+
+//reduce() function 
+
+function add(runningTotal, currentValue) {
+    return runningTotal + currentValue;
+}
+
+var nums = [1,2,3,4,5,6,7,8,9,10];
+var sum = nums.reduce(add);
+print(sum);
