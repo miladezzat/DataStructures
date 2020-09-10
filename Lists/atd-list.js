e98ac232c6817049497740499fc0e4a7eef62948
miladezzat <miladezzat.f@gmail.com>
2020-09-10 20:44:37 +0200

    List ADT (Abstract Data Type)

---
+++ Lists/atd-list.js
@@ -0,0 +1,122 @@
+'use strict';
+
+function List() {
+    this.listSize = 0;
+    this.pos = 0;
+    this.dataStore = []; // initializes an empty array to store list elements
+    this.clear = clear;
+    this.find = find;
+    this.toString = toString;
+    this.insert = insert;
+    this.append = append;
+    this.remove = remove;
+    this.end = end;
+    this.prev = prev;
+    this.next = next;
+    this.length = length;
+    this.currPos = currPos;
+    this.moveTo = moveTo;
+    this.getElement = getElement;
+    this.contains = contains;
+    this.front = front;
+}
+
+function append(element) {
+    this.dataStore[this.listSize++] = element;
+}
+
+function find(element) {
+    for (let i = 0; i < this.dataStore.length; ++i) {
+        if (this.dataStore[i] == element) {
+            return i;
+        }
+    }
+    return -1;
+}
+
+function remove(element) {
+    let foundAt = this.find(element);
+    if (foundAt > -1) {
+        this.dataStore.splice(foundAt, 1);
+        --this.listSize;
+        return true;
+    }
+    return false;
+}
+
+function length() {
+    return this.listSize;
+}
+
+function toString() {
+    return this.dataStore;
+}
+
+function insert(element, after) {
+    let insertPos = this.find(after);
+    if (insertPos > -1) {
+        this.dataStore.splice(insertPos + 1, 0, element);
+        ++this.listSize;
+        return true;
+    }
+    return false;
+}
+
+function clear() {
+    delete this.dataStore;
+    this.dataStore = [];
+    this.listSize = this.pos = 0;
+}
+
+function contains(element) {
+    for (let i = 0; i < this.dataStore.length; i++) {
+        if (this.dataStore[i] == element) {
+            return true;
+        }
+    }
+    return false;
+}
+
+function front() {
+    this.pos = 0;
+}
+function end() {
+    this.pos = this.listSize - 1;
+}
+
+function prev() {
+    if (this.pos > 0) {
+        --this.pos;
+    }
+}
+
+function next() {
+    if (this.pos < this.listSize - 1) {
+        ++this.pos;
+    }
+}
+
+function currPos() {
+    return this.pos;
+}
+
+function moveTo(position) {
+    this.pos = position;
+}
+
+function getElement() {
+    return this.dataStore[this.pos];
+}
+
+const names = new List();
+
+names.append('milad');
+names.append('ezzat');
+names.append('fahmy');
+names.append('abd');
+
+names.front();
+names.next();
+names.prev();
+
+print(names.getElement()); // displays milad
